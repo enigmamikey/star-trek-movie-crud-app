4eceacb6e0f1d8f22b31e7e088c61e284aadf7d6
Michael Burgher <mikeburgher2@gmail.com>
2025-10-07 10:40:24 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('button').addEventListener('click', click)
+let h = document.querySelector('h3')
+
+function click() {
+    h.textContent = 'This is where the movie title will go'
+}
