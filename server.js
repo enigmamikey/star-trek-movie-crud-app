7ba0da5700dd43cadc3368407d247194700dab39
Michael Burgher <mikeburgher2@gmail.com>
2025-10-07 11:08:28 -0600

    movie titles added

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const movies = [
+    'Star Trek: The Motion Picture',
+    'Star Trek II: The Wrath of Khan',
+    'Star Trek III: The Search for Spock',
+    'Star Trek IV: The Voyage Home',
+    'Star Trek V: The Final Frontier',
+    'Star Trek VI: The Voyage Home',
+    'Star Trek: Generations',
+    'Star Trek: First Contact',
+    'Star Trek: Insurrection',
+    'Star Trek: Nemesis',
+    'Star Trek',
+    'Star Trek: Into Darkness',
+    'Star Trek: Beyond',
+    'Star Trek: Section 31'
+]
+
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
